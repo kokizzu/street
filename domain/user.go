c79ba7e749aa64e31f423fded62ac398e0b690f7
Kiswono Prayogo <kiswono@gmail.com>
2023-07-07 20:34:02 +0700

    use better formula

--- domain/user.go
+++ domain/user.go
@@ -350,8 +350,13 @@ func (d *Domain) UserSearchProp(in *UserSearchPropIn) (out UserSearchPropOut) {
 	return
 }
 
-func distanceKm(lat1, long1, lat2, long2 float64) float64 {
-	return math.Acos(math.Sin(lat1)*math.Sin(lat2)+math.Cos(lat1)*math.Cos(lat2)*math.Cos(long2-long1)) * 6371
+func distanceKm(lat1, lon1, lat2, lon2 float64) float64 {
+	const r = 6371 // km
+	const p = math.Pi / 180
+
+	a := 0.5 - math.Cos((lat2-lat1)*p)/2 + math.Cos(lat1*p)*math.Cos(lat2*p)*(1-math.Cos((lon2-lon1)*p))/2
+
+	return 2 * r * math.Asin(math.Sqrt(a))
 }
 
 type (
