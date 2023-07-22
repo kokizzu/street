6ba668433cfae5b1aa4f4f93ea74c61f6496ae9c
Kiswono Prayogo <kiswono@gmail.com>
2023-07-22 20:40:09 +0700

    split backend files per route

---
+++ domain/AdminPropHistories.go
@@ -0,0 +1,197 @@
+package domain
+
+import (
+	"street/model/mProperty"
+	"street/model/mProperty/rqProperty"
+	"street/model/mProperty/wcProperty"
+	"street/model/zCrud"
+)
+
+//go:generate gomodifytags -all -add-tags json,form,query,long,msg -transform camelcase --skip-unexported -w -file AdminPropHistories.go
+//go:generate replacer -afterprefix "Id\" form" "Id,string\" form" type AdminPropHistories.go
+//go:generate replacer -afterprefix "json:\"id\"" "json:\"id,string\"" type AdminPropHistories.go
+//go:generate replacer -afterprefix "By\" form" "By,string\" form" type AdminPropHistories.go
+//go:generate farify doublequote --file AdminPropHistories.go
+
+const (
+	AdminPropHistoriesAction = `admin/propHistories`
+
+	ErrAdminPropHistoryIdNotFound = `admin prop history id not found`
+	ErrAdminPropHistorySaveFailed = `admin prop history save failed`
+)
+
+var AdminPropHistoriesMeta = zCrud.Meta{
+	Fields: []zCrud.Field{
+		{
+			Name:      mProperty.Id,
+			Label:     `Id`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeHidden,
+		},
+		{
+			Name:      mProperty.PropertyKey,
+			Label:     `Prop Key`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionKey,
+			Label:     `Trx Key`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionSign,
+			Label:     `Trx Sign`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionTime,
+			Label:     `Trx Time`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionType,
+			Label:     `Trx Type`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionDateNormal,
+			Label:     `Trx Date Normal`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.TransactionNumber,
+			Label:     `Trx Number`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.PriceNTD,
+			Label:     `Price NTD`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeNumber,
+		},
+		{
+			Name:      mProperty.PricePerUnit,
+			Label:     `Price Per Unit`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeNumber,
+		},
+		{
+			Name:      mProperty.Price,
+			Label:     `Price`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeNumber,
+		},
+		{
+			Name:      mProperty.Address,
+			Label:     `Address`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.District,
+			Label:     `District`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.Note,
+			Label:     `Note`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+	},
+}
+
+func (d *Domain) AdminPropHistories(in *AdminPropHistoriesIn) (out AdminPropHistoriesOut) {
+	defer d.InsertActionLog(&in.RequestCommon, &out.ResponseCommon)
+	sess := d.MustAdmin(in.RequestCommon, &out.ResponseCommon)
+	if sess == nil {
+		return
+	}
+
+	if in.WithMeta {
+		out.Meta = &AdminPropHistoriesMeta
+	}
+
+	switch in.Action {
+	case zCrud.ActionForm:
+		if in.PropHistory.Id <= 0 {
+			out.Meta = &AdminPropertiesMeta
+			return
+		}
+
+		ph := rqProperty.NewPropertyHistory(d.PropOltp)
+		ph.Id = in.PropHistory.Id
+		if !ph.FindById() {
+			out.SetError(400, ErrAdminPropHistoryIdNotFound)
+		}
+		out.PropHistory = ph
+	case zCrud.ActionUpsert, zCrud.ActionDelete, zCrud.ActionRestore:
+
+		ph := wcProperty.NewPropertyHistoryMutator(d.PropOltp)
+		ph.Id = in.PropHistory.Id
+		if ph.Id > 0 {
+			if !ph.FindById() {
+				out.SetError(400, ErrAdminPropHistoryIdNotFound)
+				return
+			}
+
+			if in.Action == zCrud.ActionDelete {
+				if ph.DeletedAt == 0 {
+					ph.SetDeletedAt(in.UnixNow())
+				}
+			} else if in.Action == zCrud.ActionRestore {
+				if ph.DeletedAt > 0 {
+					ph.SetDeletedAt(0)
+				}
+			}
+		} else {
+			ph.SetCreatedAt(in.UnixNow())
+		}
+
+		ph.SetPropertyKey(in.PropHistory.PropertyKey)
+		ph.SetTransactionKey(in.PropHistory.TransactionKey)
+		ph.SetTransactionSign(in.PropHistory.TransactionSign)
+		ph.SetTransactionType(in.PropHistory.TransactionType)
+		ph.SetTransactionTime(in.PropHistory.TransactionTime)
+		ph.SetTransactionDateNormal(in.PropHistory.TransactionDateNormal)
+		ph.SetTransactionNumber(in.PropHistory.TransactionNumber)
+		ph.SetPriceNtd(in.PropHistory.PriceNtd)
+		ph.SetPricePerUnit(in.PropHistory.PricePerUnit)
+		ph.SetPrice(in.PropHistory.Price)
+		ph.SetAddress(in.PropHistory.Address)
+		ph.SetDistrict(in.PropHistory.District)
+		ph.SetNote(in.PropHistory.Note)
+
+		if ph.HaveMutation() {
+			ph.SetUpdatedAt(in.UnixNow())
+			ph.SetUpdatedBy(sess.UserId)
+			if ph.Id == 0 {
+				ph.SetCreatedAt(in.UnixNow())
+			}
+		}
+		if !ph.DoUpsert() {
+			out.SetError(500, ErrAdminPropHistorySaveFailed)
+			break
+		}
+
+		out.PropHistory = &ph.PropertyHistory
+
+		if in.Pager.Page == 0 {
+			break
+		}
+		fallthrough
+	case zCrud.ActionList:
+		ph := rqProperty.NewPropertyHistory(d.PropOltp)
+		out.PropHistories = ph.FindByPagination(&AdminPropHistoriesMeta, &in.Pager, &out.Pager)
+	}
+
+	return
+}
--- domain/AdminUsers.go
+++ domain/AdminUsers.go
@@ -3,16 +3,15 @@ package domain
 import (
 	"street/model/mAuth"
 	"street/model/mAuth/rqAuth"
-	"street/model/mAuth/saAuth"
 	"street/model/mAuth/wcAuth"
 	"street/model/zCrud"
 )
 
-//go:generate gomodifytags -all -add-tags json,form,query,long,msg -transform camelcase --skip-unexported -w -file admin.go
-//go:generate replacer -afterprefix "Id\" form" "Id,string\" form" type admin.go
-//go:generate replacer -afterprefix "json:\"id\"" "json:\"id,string\"" type admin.go
-//go:generate replacer -afterprefix "By\" form" "By,string\" form" type admin.go
-//go:generate farify doublequote --file admin.go
+//go:generate gomodifytags -all -add-tags json,form,query,long,msg -transform camelcase --skip-unexported -w -file AdminUsers.go
+//go:generate replacer -afterprefix "Id\" form" "Id,string\" form" type AdminUsers.go
+//go:generate replacer -afterprefix "json:\"id\"" "json:\"id,string\"" type AdminUsers.go
+//go:generate replacer -afterprefix "By\" form" "By,string\" form" type AdminUsers.go
+//go:generate farify doublequote --file AdminUsers.go
 
 type (
 	AdminUsersIn struct {
@@ -209,42 +208,5 @@ func (d *Domain) AdminUsers(in *AdminUsersIn) (out AdminUsersOut) {
 		out.Users = r.FindByPagination(&AdminUsersMeta, &in.Pager, &out.Pager)
 	}
 
-	return
-}
-
-type (
-	AdminDashboardIn struct {
-		RequestCommon
-	}
-
-	AdminDashboardOut struct {
-		ResponseCommon
-
-		RegisteredUserTotal int64 `json:"registeredUserTotal" form:"registeredUserTotal" query:"registeredUserTotal" long:"registeredUserTotal" msg:"registeredUserTotal"`
-
-		RequestsPerDate   map[string]int `json:"requestsPerDate" form:"requestsPerDate" query:"requestsPerDate" long:"requestsPerDate" msg:"requestsPerDate"`
-		UniqueUserPerDate map[string]int `json:"uniqueUserPerDate" form:"uniqueUserPerDate" query:"uniqueUserPerDate" long:"uniqueUserPerDate" msg:"uniqueUserPerDate"`
-		UniqueIpPerDate   map[string]int `json:"uniqueIpPerDate" form:"uniqueIpPerDate" query:"uniqueIpPerDate" long:"uniqueIpPerDate" msg:"uniqueIpPerDate"`
-
-		CountPerActionsPerDate map[string]map[string]int `json:"countPerActionsPerDate" form:"countPerActionsPerDate" query:"countPerActionsPerDate" long:"countPerActionsPerDate" msg:"countPerActionsPerDate"`
-	}
-)
-
-const (
-	AdminDashboardAction = `admin/dashboard`
-)
-
-func (d *Domain) AdminDashboard(in *AdminDashboardIn) (out AdminDashboardOut) {
-	defer d.InsertActionLog(&in.RequestCommon, &out.ResponseCommon)
-
-	rq := rqAuth.NewUsers(d.AuthOltp)
-	out.RegisteredUserTotal = rq.Total()
-
-	sa := saAuth.NewActionLogs(d.AuthOlap)
-	out.RequestsPerDate = sa.StatRequestsPerDate()
-	out.UniqueUserPerDate = sa.StatUniqueUserPerDate()
-	out.UniqueIpPerDate = sa.StatUniqueIpPerDate()
-
-	out.CountPerActionsPerDate = sa.StatPerActionsPerDate()
 	return
 }
