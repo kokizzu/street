e1e767dc77330468f5ff7e2f0e4f6bd4ebc70dcf
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-08-14 03:00:27 +0800

    test 2

--- deploy/backup_clickhouse.sh
+++ deploy/backup_clickhouse.sh
@@ -17,13 +17,13 @@ BACKUP_FILE_NAME="clickhouse_$(date '+%Y%m%d_%H%M%S').tgz"
 PROJECT_DIR="root/dev/street"
 
 # Where a backup file is stored on the remote server
-BACKUP_FILE_DIR=root/$BACKUP_FILE_NAME
+BACKUP_FILE_DIR=$PROJECT_DIR/tmp/$BACKUP_FILE_NAME
 
 # Where a backup file will be store in local machine
 TARGET_LOCAL_DIR="../tmp"
 
 # Run a remote script on a local machine over ssh
-ssh -p $SSH_PORT -i $SSH_PRIVATE_KEY $SERVER_USER@$SERVER_HOST 'bash' $PROJECT_DIR/deploy/clickhouse_backup_run_on_server.sh
+ssh -p $SSH_PORT -i $SSH_PRIVATE_KEY $SERVER_USER@$SERVER_HOST 'bash' $PROJECT_DIR/deploy/backup_clickhouse_on_server.sh
 
 # Copy backup file from remote server to local machine
 rsync --remove-source-files -r -e "ssh -p ${SSH_PORT} -i ${SSH_PRIVATE_KEY}" $SERVER_USER@$SERVER_HOST:$BACKUP_FILE_DIR $TARGET_LOCAL_DIR
