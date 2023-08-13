ddeb734820d7ec6ca2550aaab869f447c9d619dc
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-08-14 03:12:12 +0800

    test 4

--- deploy/backup_clickhouse.sh
+++ deploy/backup_clickhouse.sh
@@ -9,15 +9,11 @@ SSH_PORT=22
 # Define SSH private key
 SSH_PRIVATE_KEY="~/.ssh/habi"
 
-# Define name for backup file, make sure it's match
-# with the file name that created with clickhouse_backup_run_on_server.sh
-BACKUP_FILE_NAME="clickhouse_$(date '+%Y%m%d_%H%M%S').tgz"
+# Backup file name that created with clickhouse_backup_run_on_server.sh
+BACKUP_FILE_NAME="/root/clickhouse_$(date '+%Y%m%d_%H%M%S').tgz"
 
 # Project directory on the remote server
 PROJECT_DIR="/root/dev/street"
-
-# Where a backup file is stored on the remote server
-BACKUP_FILE_DIR=$PROJECT_DIR/tmp/$BACKUP_FILE_NAME
 
 # Where a backup file will be store in local machine
 TARGET_LOCAL_DIR="../tmp"
@@ -26,4 +22,4 @@ TARGET_LOCAL_DIR="../tmp"
 ssh -p $SSH_PORT -i $SSH_PRIVATE_KEY $SERVER_USER@$SERVER_HOST 'bash' $PROJECT_DIR/deploy/backup_clickhouse_on_server.sh
 
 # Copy backup file from remote server to local machine
-rsync --remove-source-files -r -e "ssh -p ${SSH_PORT} -i ${SSH_PRIVATE_KEY}" $SERVER_USER@$SERVER_HOST:$BACKUP_FILE_DIR $TARGET_LOCAL_DIR
+rsync --remove-source-files -r -e "ssh -p ${SSH_PORT} -i ${SSH_PRIVATE_KEY}" $SERVER_USER@$SERVER_HOST:$BACKUP_FILE_NAME $TARGET_LOCAL_DIR
