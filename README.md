0cebcf462e36620b69fcd2b6fac1f9a28758ada1
Kiswono Prayogo <kiswono@gmail.com>
2023-08-21 03:12:09 +0800

    Backup/restore script (#9)
    * test backup/restore
    
    * decrease tarantool vynyl memory
    
    * [test] create backup file for clickhouse
    
    * backup clickhouse
    
    * assign execute permission to backup script
    
    * test
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test 5
    
    * edit date format
    
    ---------
    
    Co-authored-by: Ahmad Habibi <ahmadhabibi7159@gmail.com>

--- README.md
+++ README.md
@@ -64,6 +64,18 @@ caddy run # foreground
 caddy start # background
 ```
 
+## Execute Tarantool on Docker
+
+```shell
+docker exec -it street_tarantool1_1 tarantoolctl connect userT:passT@127.0.0.1:3301
+```
+
+## Execute Clickhouse on Docker
+
+```shell
+docker exec -it street_clickhouse1_1 clickhouse-client -u userC
+```
+
 ## Generate ORM
 
 ```shell
---
+++ deploy/backup_clickhouse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+# Remote server information
+SERVER_USER="root"
+SERVER_HOST="203.194.113.211"
+SSH_PORT=22
+
+# Define SSH private key
+SSH_PRIVATE_KEY="~/.ssh/habi"
+
+# Backup file name that created with clickhouse_backup_run_on_server.sh
+BACKUP_FILE_NAME="/root/clickhouse_backup.tgz"
+
+# Project directory on the remote server
+PROJECT_DIR="/root/dev/street"
+
+# Where a backup file will be store in local machine
+TARGET_LOCAL_DIR="../tmp/clickhouse_$(date '+%Y%m%d_%H%M').tgz"
+
+# Run a remote script on a local machine over ssh
+ssh -p $SSH_PORT -i $SSH_PRIVATE_KEY $SERVER_USER@$SERVER_HOST 'bash' $PROJECT_DIR/deploy/backup_clickhouse_on_server.sh
+
+# Copy backup file from remote server to local machine
+rsync --remove-source-files -r -e "ssh -p ${SSH_PORT} -i ${SSH_PRIVATE_KEY}" $SERVER_USER@$SERVER_HOST:$BACKUP_FILE_NAME $TARGET_LOCAL_DIR
