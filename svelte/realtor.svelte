da864b4650808eafb6c89a3086edf9de75e171ac
Kiswono Prayogo <kiswono@gmail.com>
2023-08-24 04:22:17 +0800

    fix property (#16)
    * owned properties page
    
    * [on-progress] edit property
    
    ---------
    
    Co-authored-by: Ahmad Habibi <ahmadhabibi7159@gmail.com>

--- svelte/realtor.svelte
+++ svelte/realtor.svelte
@@ -11,9 +11,16 @@
     console.log(ownedProperties);
   });
 
-  // <!-- {JSON.stringify( property )} -->
-  // <a href='/realtor/property/{property.id}'>
-  // <span>{property.formattedAddress}</span>
+  function datetime( unixSec ) {
+    if( !unixSec ) return '';
+    const dt = new Date( unixSec * 1000 )
+    const day = dt.toLocaleDateString('default', {weekday: 'long'});
+    const date = dt.getDay();
+    const month = dt.toLocaleDateString('default', { month: 'long'});
+    const year = dt.getFullYear()
+    const formattedDate = `${day}, ${date} ${month} ${year}`
+    return formattedDate
+  }
 
   let user = {/* user */};
   let segments = {/* segments */};
@@ -70,6 +77,126 @@
                     </div>
                   </div>
                 </div>
+              </div>
+              <div class="property_secondary">
+                <div class="feature_number">
+                  <div class="feature_item">
+                    <b>{property.numberOfFloors || '0'}</b>
+                    <p>FLoors</p>
+                  </div>
+                  <div class="feature_item">
+                    <b>{property.bathroom || '0'}</b>
+                    <p>Bathroom</p>
+                  </div>
+                  <div class="feature_item">
+                    <b>{property.bedroom || '0'}</b>
+                    <p>Bedroom</p>
+                  </div>
+                  <div class="feature_item">
+                    <b>{property.sizeM2 || '0'} M2</b>
+                    <p>Size</p>
+                  </div>
+                </div>
+              </div>
+              <table class="property_table">
+                <tbody>
+                  <tr>
+                    <td class="name">Property ID</td>
+                    <td class="value">{property.id || '-'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Created At</td>
+                    <td class="value">{datetime(property.createdAt) || '0'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Updated At</td>
+                    <td class="value">{datetime(property.updatedAt) || '0'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Building Lamination</td>
+                    <td class="value">{property.buildingLamination || '-'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Construct Completed Date</td>
+                    <td class="value">{datetime(property.constructCompletedDate) || '-'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Updated At</td>
+                    <td class="value">{datetime(property.updatedAt) || '0'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Main Use</td>
+                    <td class="value">{property.mainUse || '-'}</td>
+                  </tr>
+                  <tr>
+                    <td class="name">Note</td>
+                    <td class="value">{property.note || '-'}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="property_floors">
+                <h3>FLOORS</h3>
+                {#if property.floorList.length}
+                  <div class="floor_lists">
+                  {#each property.floorList as floors}
+                    <div class="floor_item">
+                      <div class="left">
+                        <h5>
+                          {floors.type==='basement' ? floors.type : `${floors.type} #${floors.floor}`}
+                        </h5>
+                        <!-- TODO: currently room list only 1 object, fix Tarantool to accept array -->
+                        {#if floors.rooms}
+                          <div class="room_lists">
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                            <div class="room_item">
+                              <span>{@html floors.rooms.name || '-'}</span>
+                              <span>{@html floors.rooms.sizeM2 || '-'} M2</span>
+                            </div>
+                          </div>
+                        {/if}
+                      </div>
+
+                      <div class="floor_plan_image">
+                        {#if floors.planImageUrl === ''}
+                          <span>
+                            <i class='gg-image'></i>
+                            <p>No Image</p>
+                          </span>
+                        {:else}
+                          <img src={floors.planImageUrl} alt="" />
+                        {/if}
+                      </div>
+                    </div>
+                  {/each}
+                  </div>
+                {:else}
+                  <div class="no_floors">
+                    <p>No Floors</p>
+                  </div>
+                {/if}
               </div>
             </div>
           {/each}
@@ -149,11 +276,12 @@
     padding: 15px;
     display: flex;
     flex-direction: column;
+    gap: 20px;
   }
   .property .property_main {
     display: flex;
     flex-direction: row;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
     align-content: flex-start;
     justify-content: flex-start;
     align-items: flex-start;
@@ -241,5 +369,121 @@
   }
   .property .property_main .property_info .col2 .address i {
     color: #f97316;
+  }
+
+  .property .property_secondary {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+  .property .property_secondary .feature_number {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+  }
+  .property .property_secondary .feature_number .feature_item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+  }
+  .property .property_secondary p {
+    margin: 0;
+  }
+  .property .property_secondary b {
+    font-size: 22px;
+  }
+  
+  .property .property_table {
+    border-collapse: collapse;
+    font-size: 14px;
+    width: 100%;
+    color: #475569;
+    border: 1px solid #cbd5e1;
+  }
+  .property .property_table .name {
+    width: 210px;
+    max-width: 210px;
+    font-weight: 700;
+  }
+  .property .property_table .name,
+  .property .property_table .value {
+    border-top: 1px solid #cbd5e1;
+    border-bottom: 1px solid #cbd5e1;
+    padding: 12px;
+  }
+
+  .property .property_floors {
+    width: 70%;
+    margin: 0 auto;
+  }
+
+  .property .property_floors h3 {
+    font-size: 22px;
+    margin: 0 0 15px 0;
+    text-align: center;
+  }
+  .property .property_floors .floor_lists {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .property .property_floors .floor_lists .floor_item {
+    display: flex;
+    align-items: flex-start;
+    gap: 35px;
+  }
+  .property .property_floors .floor_lists .floor_item .left {
+    flex-grow: 1;
+  }
+  .property .property_floors .floor_lists .floor_item .left h5 {
+    font-size: 22px;
+    text-transform: capitalize;
+    margin: 0 0 15px 0;
+    width: 100%;
+  }
+  .property .property_floors .floor_lists .floor_lists .floor_item .left .room_lists {
+    font-size: 14px;
+    width: 100%;
+    color: #475569;
+    border: 1px solid #cbd5e1;
+  }
+  .property .property_floors .floor_lists .floor_item .left .room_lists .room_item {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    text-transform: capitalize;
+    border-bottom: 1px solid #cbd5e1;
+    padding: 5px 0;
+  }
+  .property .property_floors .floor_lists .floor_item .floor_plan_image {
+    width: 240px;
+    height: 140px;
+    position      : relative;
+    border-radius : 8px;
+    overflow      : hidden;
+    border: 1px solid #cbd5e1;
+    margin-top: 45px;
+  }
+  .property .property_floors .floor_lists .floor_item .floor_plan_image img {
+    width      : 100%;
+    height     : 100%;
+    object-fit : cover;
+  }
+  .property .property_floors .floor_lists .floor_item .floor_plan_image span {
+    border-radius    : 8px;
+    object-fit       : cover;
+    width            : 100%;
+    height           : 100%;
+    display          : flex;
+    flex-direction: column;
+    gap: 8px;
+    justify-content  : center;
+    align-items      : center;
+  }
+  .property .property_floors .floor_lists .floor_item .floor_plan_image span p {
+    margin: 0;
   }
 </style>
