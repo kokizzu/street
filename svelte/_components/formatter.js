2058b16a3a4fa9e559670c926276661df22775aa
Kiswono Prayogo <kiswono@gmail.com>
2023-08-29 00:02:36 +0700

    formatting

--- svelte/_components/formatter.js
+++ svelte/_components/formatter.js
@@ -1,37 +1,37 @@
 function datetime( unixSec ) {
   if( !unixSec ) return '-';
-  const dt = new Date( unixSec * 1000 )
-  const options = { day: '2-digit', month: 'long', year: 'numeric' };
-  const formattedDate = dt.toLocaleDateString(undefined, options);
+  const dt = new Date( unixSec * 1000 );
+  const options = {day: '2-digit', month: 'long', year: 'numeric'};
+  const formattedDate = dt.toLocaleDateString( undefined, options );
   // return dt.substring( 0, 10 ) + ' ' + dt.substring( 11, 16 );
-  return formattedDate
+  return formattedDate;
 }
 
 function datetime2( unixSec ) {
   if( !unixSec ) return '-';
-
-  const dt = new Date(unixSec * 1000);
+  
+  const dt = new Date( unixSec * 1000 );
   const year = dt.getFullYear();
-  const month = dt.toLocaleDateString('default', { month: 'long'}); // String(dt.getMonth() + 1).padStart(2, '0');
-  const day = dt.toLocaleDateString('default', {weekday: 'long'});
-  const date = String(dt.getDate()).padStart(2, '0');
-  const hours = String(dt.getHours()).padStart(2, '0');
-  const minutes = String(dt.getMinutes()).padStart(2, '0');
-  const seconds = String(dt.getSeconds()).padStart(2, '0');
-
+  const month = dt.toLocaleDateString( 'default', {month: 'long'} ); // String(dt.getMonth() + 1).padStart(2, '0');
+  const day = dt.toLocaleDateString( 'default', {weekday: 'long'} );
+  const date = String( dt.getDate() ).padStart( 2, '0' );
+  const hours = String( dt.getHours() ).padStart( 2, '0' );
+  const minutes = String( dt.getMinutes() ).padStart( 2, '0' );
+  const seconds = String( dt.getSeconds() ).padStart( 2, '0' );
+  
   const formattedDate = `${day}, ${date} ${month} ${year} - ${hours}:${minutes}:${seconds}`;
-  return formattedDate
+  return formattedDate;
 }
 
 function formatPrice( price, currency ) {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat( 'en-US', {
     style: 'currency',
     currency: currency,
-  }).format(price);
+  } ).format( price );
 }
 
 module.exports = {
   datetime: datetime,
   datetime2: datetime2,
-  formatPrice: formatPrice
+  formatPrice: formatPrice,
 };
--- svelte/admin/properties.svelte
+++ svelte/admin/properties.svelte
@@ -9,11 +9,11 @@
   import HiSolidEye from 'svelte-icons-pack/hi/HiSolidEye';
   import { AdminProperties, UserPropHistory } from '../jsApi.GEN';
   import { datetime2 } from '_components/formatter';
-
+  
   import Icon from 'svelte-icons-pack/Icon.svelte';
   import FaSolidPlusCircle from 'svelte-icons-pack/fa/FaSolidPlusCircle';
   import ModalDialog from '../_components/ModalDialog.svelte';
-
+  
   let segments = {/* segments */};
   let fields = [/* fields */];
   let properties = [/* properties */] || [];
@@ -23,78 +23,78 @@
     {
       icon: HiSolidEye,
       tooltip: 'Show Property History',
-      onClick: function (item) {
-        let propertyKey = item[1]; // property key for taiwan data
+      onClick: function( item ) {
+        let propertyKey = item[ 1 ]; // property key for taiwan data
         UserPropHistory(
           {
             propertyKey: propertyKey,
           },
-          function (res) {
-            if (res.error) alert(res.error);
+          function( res ) {
+            if( res.error ) alert( res.error );
             propHistoryModal.showModal();
             currentPropHistory = res.history || [];
-            console.log(res.history)
-          }
+            console.log( res.history );
+          },
         );
       },
     },
   ];
-
-  $: console.log('properties=', properties);
-
+  
+  $: console.log( 'properties=', properties );
+  
   let propHistoryModal = ModalDialog;
-
+  
   // return true if got error
-  function handleResponse(res) {
-    console.log(res);
-    if (res.error) {
+  function handleResponse( res ) {
+    console.log( res );
+    if( res.error ) {
       // TODO: growl error
-      alert(res.error);
+      alert( res.error );
       return true;
     }
-    if (res.properties && res.properties.length) properties = res.properties;
-    if (res.pager && res.pager.page) pager = res.pager;
-    if (res.pager && res.pager.filters && !res.properties) properties = []; // if nothing found but filter exists, clear table
+    if( res.properties && res.properties.length ) properties = res.properties;
+    if( res.pager && res.pager.page ) pager = res.pager;
+    if( res.pager && res.pager.filters && !res.properties ) properties = []; // if nothing found but filter exists, clear table
   }
-
-  async function refreshTableView(pagerIn) {
+  
+  async function refreshTableView( pagerIn ) {
     // console.log( 'pagerIn=',pagerIn );
     await AdminProperties(
       {
         pager: pagerIn,
         action: 'list',
       },
-      function (res) {
-        handleResponse(res);
-      }
+      function( res ) {
+        handleResponse( res );
+      },
     );
   }
-
+  
   let form = ModalForm; // for lookup
-
-  async function editRow(id, row) {
+  
+  async function editRow( id, row ) {
     await AdminProperties(
       {
-        property: { id },
+        property: {id},
         action: 'form',
       },
-      function (res) {
-        if (!handleResponse(res)) form.showModal(res.property);
-      }
+      function( res ) {
+        if( !handleResponse( res ) ) form.showModal( res.property );
+      },
     );
   }
-
+  
   function addRow() {
-    form.showModal({ id: '' });
+    form.showModal( {id: ''} );
   }
-
-  async function saveRow(action, row) {
-    let property = { ...row };
-    if (!property.id) property.id = '0';
-    console.log(property);
+  
+  async function saveRow( action, row ) {
+    let property = {...row};
+    if( !property.id ) property.id = '0';
+    console.log( property );
     try {
-      property.coord = JSON.parse('[' + property.coord + ']');
-    } catch (e) {
+      property.coord = JSON.parse( '[' + property.coord + ']' );
+    } catch( e ) {
       property.coord = [0, 0];
     }
     await AdminProperties(
@@ -103,24 +103,24 @@
         action: action,
         pager: pager, // force refresh page, will be slow
       },
-      function (res) {
-        if (handleResponse(res)) {
-          return form.setLoading(false); // has error
+      function( res ) {
+        if( handleResponse( res ) ) {
+          return form.setLoading( false ); // has error
         }
         form.hideModal(); // success
-      }
+      },
     );
   }
 </script>
 
-<section class="dashboard">
+<section class='dashboard'>
   <Menu access={segments} />
-  <div class="dashboard_main_content">
+  <div class='dashboard_main_content'>
     <ProfileHeader />
     <AdminSubMenu />
-    <div class="content">
-      <ModalForm {fields} rowType="Property" bind:this={form} onConfirm={saveRow} />
-      <section class="tableview_container">
+    <div class='content'>
+      <ModalForm {fields} rowType='Property' bind:this={form} onConfirm={saveRow} />
+      <section class='tableview_container'>
         <TableView
           {fields}
           {extraActions}
@@ -133,8 +133,8 @@
           onRefreshTableView={refreshTableView}
           onEditRow={editRow}
         >
-          <button on:click={addRow} class="add_button">
-            <Icon size={18} color="#FFFF" src={FaSolidPlusCircle} />
+          <button on:click={addRow} class='add_button'>
+            <Icon size={18} color='#FFFF' src={FaSolidPlusCircle} />
             <span>Add</span>
           </button>
         </TableView>
@@ -142,67 +142,86 @@
     </div>
     <Footer />
   </div>
-  <ModalDialog bind:this={propHistoryModal} title="Property History">
-    <div slot="content" >
+  <ModalDialog bind:this={propHistoryModal} title='Property History'>
+    <div slot='content'>
       {#if currentPropHistory && currentPropHistory.length}
         {#each currentPropHistory as row}
-          <div class="prop_item">
-            <div class="row">
-              <p class="name">Property ID</p><span>:</span><p class="value">{row.id}</p>
+          <div class='prop_item'>
+            <div class='row'>
+              <p class='name'>Property ID</p><span>:</span>
+              <p class='value'>{row.id}</p>
             </div>
-            <div class="row">
-              <p class="name">Created At</p><span>:</span><p class="value">{datetime2(row.createdAt) || '-'}</p>
+            <div class='row'>
+              <p class='name'>Created At</p><span>:</span>
+              <p class='value'>{datetime2( row.createdAt ) || '-'}</p>
             </div>
-            <div class="row">
-              <p class="name">Created By</p><span>:</span><p class="value">{row.createdBy}</p>
+            <div class='row'>
+              <p class='name'>Created By</p><span>:</span>
+              <p class='value'>{row.createdBy}</p>
             </div>
-            <div class="row">
-              <p class="name">Deleted At</p><span>:</span><p class="value">{datetime2(row.deletedAt) || '-'}</p>
+            <div class='row'>
+              <p class='name'>Deleted At</p><span>:</span>
+              <p class='value'>{datetime2( row.deletedAt ) || '-'}</p>
             </div>
-            <div class="row">
-              <p class="name">Updated At</p><span>:</span><p class="value">{datetime2(row.updatedAt) || '-'}</p>
+            <div class='row'>
+              <p class='name'>Updated At</p><span>:</span>
+              <p class='value'>{datetime2( row.updatedAt ) || '-'}</p>
             </div>
-            <div class="row">
-              <p class="name">Updated By</p><span>:</span><p class="value">{datetime2(row.updatedBy)}</p>
+            <div class='row'>
+              <p class='name'>Updated By</p><span>:</span>
+              <p class='value'>{datetime2( row.updatedBy )}</p>
             </div>
-            <div class="row">
-              <p class="name">Address</p><span>:</span><p class="value">{row.address}</p>
+            <div class='row'>
+              <p class='name'>Address</p><span>:</span>
+              <p class='value'>{row.address}</p>
             </div>
-            <div class="row">
-              <p class="name">District</p><span>:</span><p class="value">{row.district}</p>
+            <div class='row'>
+              <p class='name'>District</p><span>:</span>
+              <p class='value'>{row.district}</p>
             </div>
-            <div class="row">
-              <p class="name">Price</p><span>:</span><p class="value">{row.price}</p>
+            <div class='row'>
+              <p class='name'>Price</p><span>:</span>
+              <p class='value'>{row.price}</p>
             </div>
-            <div class="row">
-              <p class="name">Price NTD</p><span>:</span><p class="value">{row.priceNtd}</p>
+            <div class='row'>
+              <p class='name'>Price NTD</p><span>:</span>
+              <p class='value'>{row.priceNtd}</p>
             </div>
-            <div class="row">
-              <p class="name">Price per Unit</p><span>:</span><p class="value">{row.pricePerUnit}</p>
+            <div class='row'>
+              <p class='name'>Price per Unit</p><span>:</span>
+              <p class='value'>{row.pricePerUnit}</p>
             </div>
-            <div class="row">
-              <p class="name">Property Key</p><span>:</span><p class="value">{row.propertyKey}</p>
+            <div class='row'>
+              <p class='name'>Property Key</p><span>:</span>
+              <p class='value'>{row.propertyKey}</p>
             </div>
-            <div class="row">
-              <p class="name">Serial Number</p><span>:</span><p class="value">{row.serialNumber}</p>
+            <div class='row'>
+              <p class='name'>Serial Number</p><span>:</span>
+              <p class='value'>{row.serialNumber}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction date normal</p><span>:</span><p class="value">{row.transactionDateNormal || '-'}</p>
+            <div class='row'>
+              <p class='name'>Transaction date normal</p><span>:</span>
+              <p class='value'>{row.transactionDateNormal || '-'}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction Key</p><span>:</span><p class="value">{row.transactionKey}</p>
+            <div class='row'>
+              <p class='name'>Transaction Key</p><span>:</span>
+              <p class='value'>{row.transactionKey}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction Number</p><span>:</span><p class="value">{row.transactionNumber}</p>
+            <div class='row'>
+              <p class='name'>Transaction Number</p><span>:</span>
+              <p class='value'>{row.transactionNumber}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction Sign</p><span>:</span><p class="value">{row.transactionSign}</p>
+            <div class='row'>
+              <p class='name'>Transaction Sign</p><span>:</span>
+              <p class='value'>{row.transactionSign}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction Time</p><span>:</span><p class="value">{datetime2(row.transactionTime)}</p>
+            <div class='row'>
+              <p class='name'>Transaction Time</p><span>:</span>
+              <p class='value'>{datetime2( row.transactionTime )}</p>
             </div>
-            <div class="row">
-              <p class="name">Transaction Type</p><span>:</span><p class="value">{row.transactionType}</p>
+            <div class='row'>
+              <p class='name'>Transaction Type</p><span>:</span>
+              <p class='value'>{row.transactionType}</p>
             </div>
           </div>
         {/each}
@@ -215,31 +234,36 @@
 
 <style>
   .prop_item {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    padding: 8px;
-    border: 1px solid #cbd5e1;
-    background-color: #f1f5f9;
-    border-radius: 8px;
+    display          : flex;
+    flex-direction   : column;
+    gap              : 5px;
+    padding          : 8px;
+    border           : 1px solid #CBD5E1;
+    background-color : #F1F5F9;
+    border-radius    : 8px;
   }
+
   .prop_item .row {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 4px;
+    display        : flex;
+    flex-direction : row;
+    align-items    : center;
+    gap            : 4px;
   }
+
   .prop_item .row p {
-    margin: 0;
+    margin : 0;
   }
+
   .prop_item .row .name {
-    width: 35%;
+    width : 35%;
   }
+
   .prop_item .row .name,
   .prop_item .row span {
-    font-weight: 700;
+    font-weight : 700;
   }
+
   .prop_item .row .value {
-    flex-grow: 1;
+    flex-grow : 1;
   }
 </style>
