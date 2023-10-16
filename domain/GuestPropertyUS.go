00755492b6e120ff075ead0bf7c1d9663f1dc0be
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-10-17 02:17:40 +0800

    fix: button break, on-progress: 2 button prop version, guestprop us handler

---
+++ domain/GuestPropertyUS.go
@@ -0,0 +1,110 @@
+package domain
+
+import (
+	"street/model/mProperty"
+	"street/model/mProperty/rqProperty"
+	"street/model/zCrud"
+)
+
+//go:generate gomodifytags -all -add-tags json,form,query,long,msg -transform camelcase --skip-unexported -w -file GuestProperty.go
+//go:generate replacer -afterprefix "Id\" form" "Id,string\" form" type GuestProperty.go
+//go:generate replacer -afterprefix "json:\"id\"" "json:\"id,string\"" type GuestProperty.go
+//go:generate replacer -afterprefix "By\" form" "By,string\" form" type GuestProperty.go
+//go:generate farify doublequote --file GuestProperty.go
+
+type (
+	GuestPropertyUSIn struct {
+		RequestCommon
+		Id uint64 `json:"id,string" form:"id" query:"id" long:"id" msg:"id"`
+	}
+	GuestPropertyUSOut struct {
+		ResponseCommon
+		Property *rqProperty.Property `json:"property" form:"property" query:"property" long:"property" msg:"property"`
+		Meta     []zCrud.Field        `json:"meta" form:"meta" query:"meta" long:"meta" msg:"meta"`
+	}
+)
+
+const (
+	GuestPropertyUSAction      = `guest/property`
+	ErrGuestPropertyUSNotFound = `Property not found`
+)
+
+var (
+	GuestPropertiesUSMeta = []zCrud.Field{
+		{
+			Name:      mProperty.MainUse,
+			Label:     `Main Use / Facility`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.MainBuildingMaterial,
+			Label:     `Main Building Material`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.ConstructCompletedDate,
+			Label:     `Construct Completed Date`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.BuildingLamination,
+			Label:     `Building Lamination`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.Address,
+			Label:     `Address`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.District,
+			Label:     `District`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.Note,
+			Label:     `Note`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.Country,
+			Label:     `Country`,
+			DataType:  zCrud.DataTypeString,
+			InputType: zCrud.InputTypeText,
+		},
+		{
+			Name:      mProperty.CreatedAt,
+			Label:     `Created At`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeDateTime,
+		},
+		{
+			Name:      mProperty.UpdatedAt,
+			Label:     `Last Update`,
+			DataType:  zCrud.DataTypeInt,
+			InputType: zCrud.InputTypeDateTime,
+		},
+	}
+)
+
+func (d *Domain) GuestPropertyUS(in *GuestPropertyIn) (out GuestPropertyOut) {
+	defer d.InsertActionLog(&in.RequestCommon, &out.ResponseCommon)
+
+	r := rqProperty.NewProperty(d.PropOltp)
+	r.Id = in.Id
+	if !r.FindById() {
+		out.SetError(400, ErrGuestPropertyNotFound)
+		return
+	}
+	r.NormalizeFloorList()
+	out.Property = r
+	out.Meta = GuestPropertiesUSMeta
+	return
+}
