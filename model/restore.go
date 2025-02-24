f1a8b26a41493e36dee82cc682ce0b62bcd45714
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-02-25 00:00:24 +0800

    Backup (#99)
    * in-progress: command dump_trunc_table for dump and truncate database
    
    * in-progress: argument for each table
    
    * backup table property to LZ4-compressed JSONL
    
    * backup table propertyUS, propertyTW to LZ4-compressed JSONL
    
    * truncate table after backup to LZ4-compressed JSONL
    
    * in-progress: restore table
    
    * restore table propertyUS
    
    * restore table property/propertyTW
    
    * fix missing data from backed up files

--- model/restore.go
+++ model/restore.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"street/model/mProperty/rqProperty"
+	"street/model/mProperty"
 	"street/model/mProperty/wcProperty"
 	"strings"
 
@@ -91,140 +91,61 @@ func restoreTableProperty(conn *Tt.Adapter) error {
 				continue
 			}
 
-			if len(row) < 48 {
-				continue
+			rowsLength := len(row)
+			tableLength := len(mProperty.TarantoolTables[mProperty.TableProperty].Fields)
+			for {
+				if rowsLength < tableLength {
+					rowType := mProperty.TarantoolTables[mProperty.TableProperty].Fields[rowsLength].Type
+					rowName := mProperty.TarantoolTables[mProperty.TableProperty].Fields[rowsLength].Name
+
+					var rowDataToAppend any = ""
+
+					switch rowType {
+					case Tt.String:
+						rowDataToAppend = ""
+					case Tt.Boolean:
+						rowDataToAppend = false
+					case Tt.Double, Tt.Integer, Tt.Unsigned:
+						rowDataToAppend = 0
+					case Tt.Array:
+						rowDataToAppend = []any{0, 0}
+					default:
+						rowDataToAppend = ""
+					}
+
+					switch rowName {
+					case mProperty.Attribute, mProperty.Note:
+						rowDataToAppend = "{}"
+					}
+
+					row = append(row, rowDataToAppend)
+					rowsLength++
+					continue
+				}
+
+				break
 			}
 
-			prop := wcProperty.NewPropertyMutator(conn)
+			prop := wcProperty.NewPropertyUSMutator(conn)
+			prop.FromUncensoredArray(row)
 
 			uniqPropKey := X.ToS(row[prop.IdxUniqPropKey()])
-			serialNumber := X.ToS(row[prop.IdxSerialNumber()])
-			sizeM2 := X.ToS(row[prop.IdxSizeM2()])
-			mainUse := X.ToS(row[prop.IdxMainUse()])
-			mainBuildingMaterial := X.ToS(row[prop.IdxMainBuildingMaterial()])
-			constructCompleteDate := X.ToS(row[prop.IdxConstructCompletedDate()])
-			numberOfFloors := X.ToS(row[prop.IdxNumberOfFloors()])
-			buildingLamination := X.ToS(row[prop.IdxBuildingLamination()])
-			address := X.ToS(row[prop.IdxAddress()])
-			district := X.ToS(row[prop.IdxDistrict()])
-			note := X.ToS(row[prop.IdxNote()])
-			coord := X.ToArr(row[prop.IdxCoord()])
-			createdAt := X.ToI(row[prop.IdxCreatedAt()])
-			createdBy := X.ToU(row[prop.IdxCreatedBy()])
-			updatedAt := X.ToI(row[prop.IdxUpdatedAt()])
-			deletedAt := X.ToI(row[prop.IdxDeletedAt()])
-			formattedAddress := X.ToS(row[prop.IdxFormattedAddress()])
-			lastPrice := X.ToS(row[prop.IdxLastPrice()])
-			priceHistoriesSell := X.ToArr(row[prop.IdxPriceHistoriesSell()])
-			priceHistoriesRent := X.ToArr(row[prop.IdxPriceHistoriesRent()])
-			purpose := X.ToS(row[prop.IdxPurpose()])
-			houseType := X.ToS(row[prop.IdxHouseType()])
-			images := X.ToArr(row[prop.IdxImages()])
-			bedroom := X.ToI(row[prop.IdxBedroom()])
-			bathroom := X.ToI(row[prop.IdxBathroom()])
-			agencyFeePercent := X.ToF(row[prop.IdxAgencyFeePercent()])
-			floorList := X.ToArr(row[prop.IdxFloorList()])
-			version := X.ToS(row[prop.IdxVersion()])
-			yearBuilt := X.ToI(row[prop.IdxYearBuilt()])
-			yearRenovated := X.ToI(row[prop.IdxYearRenovated()])
-			totalSqFt := X.ToF(row[prop.IdxTotalSqft()])
-			countyName := X.ToS(row[prop.IdxCountyName()])
-			street := X.ToS(row[prop.IdxStreet()])
-			city := X.ToS(row[prop.IdxCity()])
-			state := X.ToS(row[prop.IdxState()])
-			zip := X.ToS(row[prop.IdxZip()])
-			propertyLastUpdatedDate := X.ToI(row[prop.IdxPropertyLastUpdatedDate()])
-			approvalState := X.ToS(row[prop.IdxApprovalState()])
-			countryCode := X.ToS(row[prop.IdxCountryCode()])
-			livingRoom := X.ToI(row[prop.IdxLivingroom()])
-			altitude := X.ToF(row[prop.IdxAltitude()])
-			parking := X.ToF(row[prop.IdxAltitude()])
-			depositFee := X.ToF(row[prop.IdxDepositFee()])
-			minimumDurationYear := X.ToF(row[prop.IdxMinimumDurationYear()])
-			otherFees := X.ToArr(row[prop.IdxOtherFees()])
-			imageLabels := X.ToArr(row[prop.IdxImageLabels()])
 
 			var isFoundByUniqPropKey = true
-
-			prop.UniqPropKey = uniqPropKey
 			if !prop.FindByUniqPropKey() {
-				prop.SetUniqPropKey(uniqPropKey)
 				isFoundByUniqPropKey = false
-			}
-			prop.SetSerialNumber(serialNumber)
-			prop.SetSizeM2(sizeM2)
-			prop.SetMainUse(mainUse)
-			prop.SetMainBuildingMaterial(mainBuildingMaterial)
-			prop.SetConstructCompletedDate(constructCompleteDate)
-			prop.SetNumberOfFloors(numberOfFloors)
-			prop.SetBuildingLamination(buildingLamination)
-			prop.SetAddress(address)
-			prop.SetDistrict(district)
-			prop.SetNote(note)
-			prop.SetCoord(coord)
-			prop.SetCreatedAt(createdAt)
-			prop.SetCreatedBy(createdBy)
-			prop.SetUpdatedAt(updatedAt)
-			prop.SetDeletedAt(deletedAt)
-			prop.SetFormattedAddress(formattedAddress)
-			prop.SetLastPrice(lastPrice)
-			prop.SetPriceHistoriesSell(priceHistoriesSell)
-			prop.SetPriceHistoriesRent(priceHistoriesRent)
-			prop.SetPurpose(purpose)
-			prop.SetHouseType(houseType)
-			prop.SetImages(images)
-			prop.SetBedroom(bedroom)
-			prop.SetBathroom(bathroom)
-			prop.SetAgencyFeePercent(agencyFeePercent)
-			prop.SetFloorList(floorList)
-			prop.SetVersion(version)
-			prop.SetYearBuilt(yearBuilt)
-			prop.SetYearRenovated(yearRenovated)
-			prop.SetTotalSqft(totalSqFt)
-			prop.SetCountyName(countyName)
-			prop.SetStreet(street)
-			prop.SetCity(city)
-			prop.SetState(state)
-			prop.SetZip(zip)
-			prop.SetPropertyLastUpdatedDate(propertyLastUpdatedDate)
-			prop.SetApprovalState(approvalState)
-			prop.SetCountryCode(countryCode)
-			prop.SetLivingroom(livingRoom)
-			prop.SetAltitude(altitude)
-			prop.SetParking(parking)
-			prop.SetDepositFee(depositFee)
-			prop.SetMinimumDurationYear(minimumDurationYear)
-			prop.SetOtherFees(otherFees)
-			prop.SetImageLabels(imageLabels)
-			if len(row) == 48 {
-				var attribute = rqProperty.PropertyAttribute{
-					Title:          "",
-					AgentPhone:     "",
-					AgentBio:       "",
-					BuildingSizeM2: 0,
-					YardSizeM2:     0,
-					ContactLink:    "",
-				}
-
-				attrByte, err := json.Marshal(attribute)
-				if err != nil {
-					L.LOG.Error("failed to marshal property attribute "+uniqPropKey+" : ", err)
-				} else {
-					prop.SetAttribute(string(attrByte))
-				}
-			} else {
-				attribute := X.ToS(row[prop.IdxAttribute()])
-				prop.SetAttribute(attribute)
 			}
 
 			if isFoundByUniqPropKey {
 				if !prop.DoOverwriteByUniqPropKey() {
-					L.Print("failed to overwrite property by uniqPropKey: " + uniqPropKey)
+					L.Print("failed to overwrite " + TableProperty + " by uniqPropKey: " + uniqPropKey)
 					continue
 				}
+				rowOverrideStr := color.BlueString(fmt.Sprintf("[ %d ] Updated %s '%s'", idxLine, TableProperty, uniqPropKey))
+				fmt.Println(rowOverrideStr)
 			} else {
 				if !prop.DoInsert() {
-					L.Print("failed to insert property " + uniqPropKey)
+					L.Print("failed to insert " + TableProperty + " " + uniqPropKey)
 					continue
 				}
 			}
@@ -269,140 +190,61 @@ func restoreTablePropertyUS(conn *Tt.Adapter) error {
 				continue
 			}
 
-			if len(row) < 48 {
-				continue
+			rowsLength := len(row)
+			tableLength := len(mProperty.TarantoolTables[mProperty.TablePropertyUS].Fields)
+			for {
+				if rowsLength < tableLength {
+					rowType := mProperty.TarantoolTables[mProperty.TablePropertyUS].Fields[rowsLength].Type
+					rowName := mProperty.TarantoolTables[mProperty.TablePropertyUS].Fields[rowsLength].Name
+
+					var rowDataToAppend any = ""
+
+					switch rowType {
+					case Tt.String:
+						rowDataToAppend = ""
+					case Tt.Boolean:
+						rowDataToAppend = false
+					case Tt.Double, Tt.Integer, Tt.Unsigned:
+						rowDataToAppend = 0
+					case Tt.Array:
+						rowDataToAppend = []any{0, 0}
+					default:
+						rowDataToAppend = ""
+					}
+
+					switch rowName {
+					case mProperty.Attribute, mProperty.Note:
+						rowDataToAppend = "{}"
+					}
+
+					row = append(row, rowDataToAppend)
+					rowsLength++
+					continue
+				}
+
+				break
 			}
 
 			prop := wcProperty.NewPropertyUSMutator(conn)
+			prop.FromUncensoredArray(row)
 
 			uniqPropKey := X.ToS(row[prop.IdxUniqPropKey()])
-			serialNumber := X.ToS(row[prop.IdxSerialNumber()])
-			sizeM2 := X.ToS(row[prop.IdxSizeM2()])
-			mainUse := X.ToS(row[prop.IdxMainUse()])
-			mainBuildingMaterial := X.ToS(row[prop.IdxMainBuildingMaterial()])
-			constructCompleteDate := X.ToS(row[prop.IdxConstructCompletedDate()])
-			numberOfFloors := X.ToS(row[prop.IdxNumberOfFloors()])
-			buildingLamination := X.ToS(row[prop.IdxBuildingLamination()])
-			address := X.ToS(row[prop.IdxAddress()])
-			district := X.ToS(row[prop.IdxDistrict()])
-			note := X.ToS(row[prop.IdxNote()])
-			coord := X.ToArr(row[prop.IdxCoord()])
-			createdAt := X.ToI(row[prop.IdxCreatedAt()])
-			createdBy := X.ToU(row[prop.IdxCreatedBy()])
-			updatedAt := X.ToI(row[prop.IdxUpdatedAt()])
-			deletedAt := X.ToI(row[prop.IdxDeletedAt()])
-			formattedAddress := X.ToS(row[prop.IdxFormattedAddress()])
-			lastPrice := X.ToS(row[prop.IdxLastPrice()])
-			priceHistoriesSell := X.ToArr(row[prop.IdxPriceHistoriesSell()])
-			priceHistoriesRent := X.ToArr(row[prop.IdxPriceHistoriesRent()])
-			purpose := X.ToS(row[prop.IdxPurpose()])
-			houseType := X.ToS(row[prop.IdxHouseType()])
-			images := X.ToArr(row[prop.IdxImages()])
-			bedroom := X.ToI(row[prop.IdxBedroom()])
-			bathroom := X.ToI(row[prop.IdxBathroom()])
-			agencyFeePercent := X.ToF(row[prop.IdxAgencyFeePercent()])
-			floorList := X.ToArr(row[prop.IdxFloorList()])
-			version := X.ToS(row[prop.IdxVersion()])
-			yearBuilt := X.ToI(row[prop.IdxYearBuilt()])
-			yearRenovated := X.ToI(row[prop.IdxYearRenovated()])
-			totalSqFt := X.ToF(row[prop.IdxTotalSqft()])
-			countyName := X.ToS(row[prop.IdxCountyName()])
-			street := X.ToS(row[prop.IdxStreet()])
-			city := X.ToS(row[prop.IdxCity()])
-			state := X.ToS(row[prop.IdxState()])
-			zip := X.ToS(row[prop.IdxZip()])
-			propertyLastUpdatedDate := X.ToI(row[prop.IdxPropertyLastUpdatedDate()])
-			approvalState := X.ToS(row[prop.IdxApprovalState()])
-			countryCode := X.ToS(row[prop.IdxCountryCode()])
-			livingRoom := X.ToI(row[prop.IdxLivingroom()])
-			altitude := X.ToF(row[prop.IdxAltitude()])
-			parking := X.ToF(row[prop.IdxAltitude()])
-			depositFee := X.ToF(row[prop.IdxDepositFee()])
-			minimumDurationYear := X.ToF(row[prop.IdxMinimumDurationYear()])
-			otherFees := X.ToArr(row[prop.IdxOtherFees()])
-			imageLabels := X.ToArr(row[prop.IdxImageLabels()])
 
 			var isFoundByUniqPropKey = true
-
-			prop.UniqPropKey = uniqPropKey
 			if !prop.FindByUniqPropKey() {
-				prop.SetUniqPropKey(uniqPropKey)
 				isFoundByUniqPropKey = false
-			}
-			prop.SetSerialNumber(serialNumber)
-			prop.SetSizeM2(sizeM2)
-			prop.SetMainUse(mainUse)
-			prop.SetMainBuildingMaterial(mainBuildingMaterial)
-			prop.SetConstructCompletedDate(constructCompleteDate)
-			prop.SetNumberOfFloors(numberOfFloors)
-			prop.SetBuildingLamination(buildingLamination)
-			prop.SetAddress(address)
-			prop.SetDistrict(district)
-			prop.SetNote(note)
-			prop.SetCoord(coord)
-			prop.SetCreatedAt(createdAt)
-			prop.SetCreatedBy(createdBy)
-			prop.SetUpdatedAt(updatedAt)
-			prop.SetDeletedAt(deletedAt)
-			prop.SetFormattedAddress(formattedAddress)
-			prop.SetLastPrice(lastPrice)
-			prop.SetPriceHistoriesSell(priceHistoriesSell)
-			prop.SetPriceHistoriesRent(priceHistoriesRent)
-			prop.SetPurpose(purpose)
-			prop.SetHouseType(houseType)
-			prop.SetImages(images)
-			prop.SetBedroom(bedroom)
-			prop.SetBathroom(bathroom)
-			prop.SetAgencyFeePercent(agencyFeePercent)
-			prop.SetFloorList(floorList)
-			prop.SetVersion(version)
-			prop.SetYearBuilt(yearBuilt)
-			prop.SetYearRenovated(yearRenovated)
-			prop.SetTotalSqft(totalSqFt)
-			prop.SetCountyName(countyName)
-			prop.SetStreet(street)
-			prop.SetCity(city)
-			prop.SetState(state)
-			prop.SetZip(zip)
-			prop.SetPropertyLastUpdatedDate(propertyLastUpdatedDate)
-			prop.SetApprovalState(approvalState)
-			prop.SetCountryCode(countryCode)
-			prop.SetLivingroom(livingRoom)
-			prop.SetAltitude(altitude)
-			prop.SetParking(parking)
-			prop.SetDepositFee(depositFee)
-			prop.SetMinimumDurationYear(minimumDurationYear)
-			prop.SetOtherFees(otherFees)
-			prop.SetImageLabels(imageLabels)
-			if len(row) == 48 {
-				var attribute = rqProperty.PropertyAttribute{
-					Title:          "",
-					AgentPhone:     "",
-					AgentBio:       "",
-					BuildingSizeM2: 0,
-					YardSizeM2:     0,
-					ContactLink:    "",
-				}
-
-				attrByte, err := json.Marshal(attribute)
-				if err != nil {
-					L.LOG.Error("failed to marshal property attribute "+uniqPropKey+" : ", err)
-				} else {
-					prop.SetAttribute(string(attrByte))
-				}
-			} else {
-				attribute := X.ToS(row[prop.IdxAttribute()])
-				prop.SetAttribute(attribute)
 			}
 
 			if isFoundByUniqPropKey {
 				if !prop.DoOverwriteByUniqPropKey() {
-					L.Print("failed to overwrite property by uniqPropKey: " + uniqPropKey)
+					L.Print("failed to overwrite " + TablePropertyUS + " by uniqPropKey: " + uniqPropKey)
 					continue
 				}
+				rowOverrideStr := color.BlueString(fmt.Sprintf("[ %d ] Updated %s '%s'", idxLine, TablePropertyUS, uniqPropKey))
+				fmt.Println(rowOverrideStr)
 			} else {
 				if !prop.DoInsert() {
-					L.Print("failed to insert property " + uniqPropKey)
+					L.Print("failed to insert " + TablePropertyUS + " " + uniqPropKey)
 					continue
 				}
 			}
@@ -447,140 +289,61 @@ func restoreTablePropertyTW(conn *Tt.Adapter) error {
 				continue
 			}
 
-			if len(row) < 48 {
-				continue
+			rowsLength := len(row)
+			tableLength := len(mProperty.TarantoolTables[mProperty.TablePropertyTW].Fields)
+			for {
+				if rowsLength < tableLength {
+					rowType := mProperty.TarantoolTables[mProperty.TablePropertyTW].Fields[rowsLength].Type
+					rowName := mProperty.TarantoolTables[mProperty.TablePropertyTW].Fields[rowsLength].Name
+
+					var rowDataToAppend any = ""
+
+					switch rowType {
+					case Tt.String:
+						rowDataToAppend = ""
+					case Tt.Boolean:
+						rowDataToAppend = false
+					case Tt.Double, Tt.Integer, Tt.Unsigned:
+						rowDataToAppend = 0
+					case Tt.Array:
+						rowDataToAppend = []any{0, 0}
+					default:
+						rowDataToAppend = ""
+					}
+
+					switch rowName {
+					case mProperty.Attribute, mProperty.Note:
+						rowDataToAppend = "{}"
+					}
+
+					row = append(row, rowDataToAppend)
+					rowsLength++
+					continue
+				}
+
+				break
 			}
 
 			prop := wcProperty.NewPropertyTWMutator(conn)
+			prop.FromUncensoredArray(row)
 
 			uniqPropKey := X.ToS(row[prop.IdxUniqPropKey()])
-			serialNumber := X.ToS(row[prop.IdxSerialNumber()])
-			sizeM2 := X.ToS(row[prop.IdxSizeM2()])
-			mainUse := X.ToS(row[prop.IdxMainUse()])
-			mainBuildingMaterial := X.ToS(row[prop.IdxMainBuildingMaterial()])
-			constructCompleteDate := X.ToS(row[prop.IdxConstructCompletedDate()])
-			numberOfFloors := X.ToS(row[prop.IdxNumberOfFloors()])
-			buildingLamination := X.ToS(row[prop.IdxBuildingLamination()])
-			address := X.ToS(row[prop.IdxAddress()])
-			district := X.ToS(row[prop.IdxDistrict()])
-			note := X.ToS(row[prop.IdxNote()])
-			coord := X.ToArr(row[prop.IdxCoord()])
-			createdAt := X.ToI(row[prop.IdxCreatedAt()])
-			createdBy := X.ToU(row[prop.IdxCreatedBy()])
-			updatedAt := X.ToI(row[prop.IdxUpdatedAt()])
-			deletedAt := X.ToI(row[prop.IdxDeletedAt()])
-			formattedAddress := X.ToS(row[prop.IdxFormattedAddress()])
-			lastPrice := X.ToS(row[prop.IdxLastPrice()])
-			priceHistoriesSell := X.ToArr(row[prop.IdxPriceHistoriesSell()])
-			priceHistoriesRent := X.ToArr(row[prop.IdxPriceHistoriesRent()])
-			purpose := X.ToS(row[prop.IdxPurpose()])
-			houseType := X.ToS(row[prop.IdxHouseType()])
-			images := X.ToArr(row[prop.IdxImages()])
-			bedroom := X.ToI(row[prop.IdxBedroom()])
-			bathroom := X.ToI(row[prop.IdxBathroom()])
-			agencyFeePercent := X.ToF(row[prop.IdxAgencyFeePercent()])
-			floorList := X.ToArr(row[prop.IdxFloorList()])
-			version := X.ToS(row[prop.IdxVersion()])
-			yearBuilt := X.ToI(row[prop.IdxYearBuilt()])
-			yearRenovated := X.ToI(row[prop.IdxYearRenovated()])
-			totalSqFt := X.ToF(row[prop.IdxTotalSqft()])
-			countyName := X.ToS(row[prop.IdxCountyName()])
-			street := X.ToS(row[prop.IdxStreet()])
-			city := X.ToS(row[prop.IdxCity()])
-			state := X.ToS(row[prop.IdxState()])
-			zip := X.ToS(row[prop.IdxZip()])
-			propertyLastUpdatedDate := X.ToI(row[prop.IdxPropertyLastUpdatedDate()])
-			approvalState := X.ToS(row[prop.IdxApprovalState()])
-			countryCode := X.ToS(row[prop.IdxCountryCode()])
-			livingRoom := X.ToI(row[prop.IdxLivingroom()])
-			altitude := X.ToF(row[prop.IdxAltitude()])
-			parking := X.ToF(row[prop.IdxAltitude()])
-			depositFee := X.ToF(row[prop.IdxDepositFee()])
-			minimumDurationYear := X.ToF(row[prop.IdxMinimumDurationYear()])
-			otherFees := X.ToArr(row[prop.IdxOtherFees()])
-			imageLabels := X.ToArr(row[prop.IdxImageLabels()])
 
 			var isFoundByUniqPropKey = true
-
-			prop.UniqPropKey = uniqPropKey
 			if !prop.FindByUniqPropKey() {
-				prop.SetUniqPropKey(uniqPropKey)
 				isFoundByUniqPropKey = false
-			}
-			prop.SetSerialNumber(serialNumber)
-			prop.SetSizeM2(sizeM2)
-			prop.SetMainUse(mainUse)
-			prop.SetMainBuildingMaterial(mainBuildingMaterial)
-			prop.SetConstructCompletedDate(constructCompleteDate)
-			prop.SetNumberOfFloors(numberOfFloors)
-			prop.SetBuildingLamination(buildingLamination)
-			prop.SetAddress(address)
-			prop.SetDistrict(district)
-			prop.SetNote(note)
-			prop.SetCoord(coord)
-			prop.SetCreatedAt(createdAt)
-			prop.SetCreatedBy(createdBy)
-			prop.SetUpdatedAt(updatedAt)
-			prop.SetDeletedAt(deletedAt)
-			prop.SetFormattedAddress(formattedAddress)
-			prop.SetLastPrice(lastPrice)
-			prop.SetPriceHistoriesSell(priceHistoriesSell)
-			prop.SetPriceHistoriesRent(priceHistoriesRent)
-			prop.SetPurpose(purpose)
-			prop.SetHouseType(houseType)
-			prop.SetImages(images)
-			prop.SetBedroom(bedroom)
-			prop.SetBathroom(bathroom)
-			prop.SetAgencyFeePercent(agencyFeePercent)
-			prop.SetFloorList(floorList)
-			prop.SetVersion(version)
-			prop.SetYearBuilt(yearBuilt)
-			prop.SetYearRenovated(yearRenovated)
-			prop.SetTotalSqft(totalSqFt)
-			prop.SetCountyName(countyName)
-			prop.SetStreet(street)
-			prop.SetCity(city)
-			prop.SetState(state)
-			prop.SetZip(zip)
-			prop.SetPropertyLastUpdatedDate(propertyLastUpdatedDate)
-			prop.SetApprovalState(approvalState)
-			prop.SetCountryCode(countryCode)
-			prop.SetLivingroom(livingRoom)
-			prop.SetAltitude(altitude)
-			prop.SetParking(parking)
-			prop.SetDepositFee(depositFee)
-			prop.SetMinimumDurationYear(minimumDurationYear)
-			prop.SetOtherFees(otherFees)
-			prop.SetImageLabels(imageLabels)
-			if len(row) == 48 {
-				var attribute = rqProperty.PropertyAttribute{
-					Title:          "",
-					AgentPhone:     "",
-					AgentBio:       "",
-					BuildingSizeM2: 0,
-					YardSizeM2:     0,
-					ContactLink:    "",
-				}
-
-				attrByte, err := json.Marshal(attribute)
-				if err != nil {
-					L.LOG.Error("failed to marshal property attribute "+uniqPropKey+" : ", err)
-				} else {
-					prop.SetAttribute(string(attrByte))
-				}
-			} else {
-				attribute := X.ToS(row[prop.IdxAttribute()])
-				prop.SetAttribute(attribute)
 			}
 
 			if isFoundByUniqPropKey {
 				if !prop.DoOverwriteByUniqPropKey() {
-					L.Print("failed to overwrite property by uniqPropKey: " + uniqPropKey)
+					L.Print("failed to overwrite " + TablePropertyTW + " by uniqPropKey: " + uniqPropKey)
 					continue
 				}
+				rowOverrideStr := color.BlueString(fmt.Sprintf("[ %d ] Updated %s '%s'", idxLine, TablePropertyTW, uniqPropKey))
+				fmt.Println(rowOverrideStr)
 			} else {
 				if !prop.DoInsert() {
-					L.Print("failed to insert property " + uniqPropKey)
+					L.Print("failed to insert " + TablePropertyTW + " " + uniqPropKey)
 					continue
 				}
 			}
